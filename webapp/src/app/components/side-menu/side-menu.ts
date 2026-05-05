a079d18f9e0a340fc0c584cc8da2752e18b9e15b
Gearpro17 <joelecossu117@gmail.com>
2026-05-05 23:21:05 +0200

    Home page UI - Added icons - Added css for side menu

--- webapp/src/app/components/side-menu/side-menu.ts
+++ webapp/src/app/components/side-menu/side-menu.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-side-menu',
-  imports: [],
+  imports: [
+    NgOptimizedImage
+  ],
   templateUrl: './side-menu.html',
   styleUrl: './side-menu.scss',
 })
 export class SideMenu {
-
+  DASHBOARD_IMG: string = "/dashboard.png";
+  PRODUCTS_IMG: string = "/products.png";
+  SUPPLIERS_IMG: string = "/suppliers.png";
+  MOVEMENTS_IMG: string = "/movements.png";
+  REORDER_IMG: string = "/reorder.png";
+  SETTINGS_IMG: string = "/settings.png";
+  ACCESS_IMG: string = "/access.png";
 }
